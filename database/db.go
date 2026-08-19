7e8e003c19c38d7d9f24951b70134eb5f2b34763
AgusPurnomo <agus.purnomomz@gmail.com>
2026-08-19 11:31:41 +0700

    Update db.go placeholder

--- database/db.go
+++ database/db.go
@@ -11,16 +11,13 @@ import (
 var DB *pgxpool.Pool
 
 func ConnectDB() {
-	// 1. Ambil URL dari Environment Variable
 	databaseURL := os.Getenv("DATABASE_URL")
 	
-	// 2. Jika kosong, hentikan aplikasi (jangan pernah tulis password di sini)
 	if databaseURL == "" {
 		fmt.Fprintf(os.Stderr, "Fatal Error: Environment variable DATABASE_URL tidak ditemukan!\n")
 		os.Exit(1)
 	}
 
-	// 3. Inisialisasi pool koneksi
 	var err error
 	DB, err = pgxpool.New(context.Background(), databaseURL)
 	if err != nil {
@@ -28,7 +25,6 @@ func ConnectDB() {
 		os.Exit(1)
 	}
 
-	// 4. Uji koneksi (Ping)
 	err = DB.Ping(context.Background())
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Database tidak merespons: %v\n", err)
